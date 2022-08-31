f61f1342a1fa05b6928ba3d7fdfe3b55ca5ae69e
pasan1227 <pasanratnayake@gmail.com>
2022-08-31 11:44:26 +0530

    Linking between pages

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, repellendus! Consequuntur doloremque sequi, harum rem ipsa doloribus obcaecati, nesciunt, quasi praesentium expedita nam quas aliquam officia? Sunt ab tenetur cupiditate.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, repellendus! Consequuntur doloremque sequi, harum rem ipsa doloribus obcaecati, nesciunt, quasi praesentium expedita nam quas aliquam officia? Sunt ab tenetur cupiditate.</p>
+      <Link href='/ninjas'>
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
