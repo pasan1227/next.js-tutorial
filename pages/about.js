df121bacbe88a73832dc209cbb2213b8129ae117
pasan1227 <pasanratnayake@gmail.com>
2022-08-31 11:15:27 +0530

    Initializing project

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, repellendus! Consequuntur doloremque sequi, harum rem ipsa doloribus obcaecati, nesciunt, quasi praesentium expedita nam quas aliquam officia? Sunt ab tenetur cupiditate.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, repellendus! Consequuntur doloremque sequi, harum rem ipsa doloribus obcaecati, nesciunt, quasi praesentium expedita nam quas aliquam officia? Sunt ab tenetur cupiditate.</p>
+        </div>
+     );
+}
+ 
+export default About;
